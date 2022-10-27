972da5a0a42b358777e6b6051931aa1ad6790876
lifehackerhansol <lifehacker@hansol.ca>
2022-10-27 08:38:14 -0700

    Enable Switch support

--- api.py
+++ api.py
@@ -17,15 +17,15 @@ app = FastAPI()
 
 
 @app.get("/{platform}/{inquiry}/{month}/{day}")
-async def get_mkey(platform: str, month: int, day: int, inquiry: str):
+async def get_mkey(platform: str, month: int, day: int, inquiry: str, aux: str = ""):
 
     generator = mkey.mkey_generator(debug=False)
 
-    if platform not in ["RVL", "TWL", "CTR", "WUP"]:
-        raise HTTPException(status_code=501, detail=f"{platform} is not implemented.")
+    if platform not in ["RVL", "TWL", "CTR", "WUP", "HAC"]:
+        raise HTTPException(status_code=400, detail=f"{platform} is an invalid platform.")
     master_key = None
     try:
-        master_key = generator.generate(inquiry=inquiry, month=month, day=day, device=platform)
+        master_key = generator.generate(inquiry=inquiry, month=month, day=day, device=platform, aux=aux)
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
     return {"key": master_key}
