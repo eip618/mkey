0e959a208786f9cae1b62d7366cedc6f8cfba2b4
lifehackerhansol <lifehacker@hansol.ca>
2022-10-30 16:53:25 -0700

    Split HTML and JSON endpoints

--- api.py
+++ api.py
@@ -15,30 +15,44 @@ from fastapi.responses import HTMLResponse
 import mkey
 
 
+class BadInputError(ValueError):
+    pass
+
 app = FastAPI()
 
-
-@app.get("/")
-async def get_mkey(request: Request, platform: Optional[str] = None, month: Optional[int] = None, day: Optional[int] = None, inquiry: Optional[str] = None, aux: Optional[str] = None):
-    if all(i is None for i in [platform, month, day, inquiry, aux]):
-        with open("index.html", 'r') as f:
-            return HTMLResponse(f.read())
-
-
+async def get_mkey(platform: Optional[str] = None, month: Optional[int] = None, day: Optional[int] = None, inquiry: Optional[str] = None, aux: Optional[str] = None) -> int:
     generator = mkey.mkey_generator(debug=False)
 
     if platform not in ["RVL", "TWL", "CTR", "WUP", "HAC"]:
-        raise HTTPException(status_code=400, detail=f"{platform} is an invalid platform.")
+        raise BadInputError(f"{platform} is an invalid platform.")
     master_key = None
     try:
         master_key = generator.generate(inquiry=inquiry, month=month, day=day, device=platform, aux=aux)
     except ValueError as e:
+        raise ValueError(str(e))
+    return str(master_key)
+
+
+@app.get("/api")
+async def api(platform: Optional[str] = None, month: Optional[int] = None, day: Optional[int] = None, inquiry: Optional[str] = None, aux: Optional[str] = None):
+    try:
+        ret = await get_mkey(platform, month, day, inquiry, aux)
+    except BadInputError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
+    return {"key": int(ret)}
 
 
-    contenttype = request.headers.get("Content-Type")
-    if contenttype == "application/json":
-        return {"key": master_key}
-    else:
+@app.get("/")
+async def webpage(platform: Optional[str] = None, month: Optional[int] = None, day: Optional[int] = None, inquiry: Optional[str] = None, aux: Optional[str] = None):
+    if all(i is None for i in [platform, month, day, inquiry, aux]):
         with open("index.html", 'r') as f:
-            return HTMLResponse(f.read().replace("<h4>DSi/ 3DS / Wii / Wii U / Switch parental controls master key generator.<br>", f"<h3>Your key is {master_key}.</h3>"))
+            return HTMLResponse(f.read())
+
+    with open("index.html", 'r') as f:
+        try:
+            ret = await get_mkey(platform, month, day, inquiry, aux)
+        except ValueError as e:
+            return HTMLResponse(f.read().replace("<h4>DSi/ 3DS / Wii / Wii U / Switch parental controls master key generator.<br>", f"<h3>Error: {str(e)}</h3>"))
+        return HTMLResponse(f.read().replace("<h4>DSi/ 3DS / Wii / Wii U / Switch parental controls master key generator.<br>", f"<h3>Your key is {ret}.</h3>"))
